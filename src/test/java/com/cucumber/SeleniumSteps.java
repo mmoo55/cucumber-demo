b02645e4e5fd8af4fb93e8f5348ef960b7b8d336
mmoo55 <g.gerson51@gmail.com>
2022-12-14 01:25:38 -0400

    Changing chrome for firefox

--- src/test/java/com/cucumber/SeleniumSteps.java
+++ src/test/java/com/cucumber/SeleniumSteps.java
@@ -15,19 +15,19 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class SeleniumSteps {
-    /*private final WebDriver driver = new FirefoxDriver();*/
-    private WebDriver driver;
+    private final WebDriver driver = new FirefoxDriver();
+    //private WebDriver driver;
 
-    public WebDriver getDriver(){
+    /*public WebDriver getDriver(){
         System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver");
         driver = new ChromeDriver();
         return driver;
-    }
+    }*/
     @Given("I am on the Google search page")
     public void I_visit_google() {
         //System.setProperty("webdriver.chrome.driver","C:\\Users\\Win10Pro\\Downloads\\chromedriver.exe");
         //driver = new ChromeDriver();
-        getDriver();
+        //getDriver();
         driver.get("https://www.google.com");
     }
 
@@ -53,6 +53,7 @@ public class SeleniumSteps {
 
     @After()
     public void closeBrowser() {
-        getDriver().quit();
+        driver.quit();
+        /*getDriver().quit();*/
     }
 }
